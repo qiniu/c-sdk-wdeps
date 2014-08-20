ec92617d5313dfe087588d00bb3dfadb63f2828b
Liang Tao <amethyst.black@gmail.com>
2014-08-20 22:07:00 +0800

    修正 WIN32 环境下无法读取大于 2G 的文件的问题。

--- emu-posix/emu_posix.h
+++ emu-posix/emu_posix.h
@@ -32,9 +32,17 @@ typedef _W64 int ssize_t;
 #define Qiniu_Posix_Handle			HANDLE
 #define Qiniu_Posix_InvalidHandle	INVALID_HANDLE_VALUE
 
+typedef _int64 Emu_Off_T;
+typedef struct _Emu_FileInfo {
+    Emu_Off_T       st_size;    /* total size, in bytes */
+    time_t          st_atime;   /* time of last access */
+    time_t          st_mtime;   /* time of last modification */
+    time_t          st_ctime;   /* time of last status change */
+} Emu_FileInfo;
+
 Qiniu_Posix_Handle Qiniu_Posix_Open(const char* file, int oflag, int mode);
-ssize_t Qiniu_Posix_Pread(Qiniu_Posix_Handle fd, void* buf, size_t nbytes, off_t offset);
-int Qiniu_Posix_Fstat(Qiniu_Posix_Handle fd, struct stat* buf);
+ssize_t Qiniu_Posix_Pread2(Qiniu_Posix_Handle fd, void* buf, size_t nbytes, Emu_Off_T offset);
+int Qiniu_Posix_Fstat2(Qiniu_Posix_Handle fd, Emu_FileInfo* buf);
 int Qiniu_Posix_Close(Qiniu_Posix_Handle fd);
 
 /*============================================================================*/
